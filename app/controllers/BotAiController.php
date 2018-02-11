ffafa1e215e7f1cce6b1978431bb12ae0832ce9e
Jimmy Wang <jimmywang2142@gmail.com>
2018-02-11 11:11:08 -0800

    fixed ai

--- app/controllers/BotAiController.php
+++ app/controllers/BotAiController.php
@@ -11,10 +11,15 @@ class BotAiController extends Controller
     private $realFlag = false;
     private $safeFlag = false;
     private $optionsFlag = false;
+    private $casual = array(
+        "How's your day?",
+        "What kind of music do you like?",
+        "Have you heard about that car in space?"
+    );
 
     public function read(Request $request){
         $name = $request->name;
-        $response = logic($name);
+        $response = $this->logic($name);
         return response()->json(array('response' => $response));
     }
 
@@ -23,13 +28,13 @@ class BotAiController extends Controller
         $text = str_replace("'","",$request);
         $text = strtolower($text);
 
-        if($realFlag){
+        if($this->realFlag){
             if(strpos($text, 'continue') !== false){
-                $realFlag = false;
+                $this->realFlag = false;
                 $response = "Ok, I will keep you company.";
             }
             else if(strpos($text, 'person') !== false){
-                $realFlag = false;
+                $this->realFlag = false;
                 $response = "Fraser Valley Help Line: 604-852-9099";
             }
             else{
@@ -37,13 +42,13 @@ class BotAiController extends Controller
             }
         }
 
-        else if($safeFlag){
+        else if($this->safeFlag){
             if(strpos($text, 'yes')){
-                $safeFlag = false;
+                $this->safeFlag = false;
                 $response = "Dialing 911 now...";
             }
             else if(strpos($text, 'no')){
-                $safeFlag = false;
+                $this->safeFlag = false;
                 $response = "Ok, I will keep you company.";
             }
             else{
@@ -51,10 +56,10 @@ class BotAiController extends Controller
             }
         }
 
-        else if($optionsFlag){
-            $optionsFlag = false;
+        else if($this->optionsFlag){
+            $this->optionsFlag = false;
             if(strpos($text, 'human')){
-                $realFlag = true;
+                $this->realFlag = true;
             }
             else if(strpos($text, 'counselor')){
                 $response = "Phone: 604-827-5180";
@@ -69,17 +74,21 @@ class BotAiController extends Controller
 
         else if(strpos($text, 'not sure') !== false or strpos($text, 'dont know') !== false){
             $response = "Would you rather continue anonymously exploring your options with me or talk to a real person? [Continue/Person]";
-            $realFlag = true;
+            $this->realFlag = true;
         }
 
         else if(strpos($text, 'afraid') !== false or strpos($text, 'scared') !== false){
             $response = "I’m sorry this happened to you. It’s not your fault and it shouldn’t have happened. Are you in a safe place now? [Yes/No]";
-            $realFlag = true;
+            $this->realFlag = true;
         }
 
         else if(strpos($text, 'my options') !== false){
             $response = "Sure, let’s talk about options.  I can tell you about resources you can access and what services they will provide.\nWould you like to talk to a real person? [Person]\nWould you like to book an appointment with a counselor? [Counselor]\nWould you like to get in touch with a support group? [Support]";
-            $optionsFlag = true;
+            $this->optionsFlag = true;
+        }
+        else{
+            $random = rand ( 0 , 2 );
+            $response = $this->casual[$random];
         }
 
         return $response;
